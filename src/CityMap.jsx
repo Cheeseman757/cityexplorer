5820446af807755c32e60456812b9009e2472e8a
Justin Cheeseman <156013482+Cheeseman757@users.noreply.github.com>
2024-03-28 15:56:57 -0700

    Update CityMap.jsx

--- src/CityMap.jsx
+++ src/CityMap.jsx
@@ -6,7 +6,7 @@ function CityMap(props) {
         <>
             <h3>{props.cityData.display_name}</h3>
             <p>{`Latitude: ${props.cityData.lat}, Longitude: ${props.cityData.lon}`}</p>
-            <img src={`https://maps.locationiq.com/v3/staticmap?key=${API_KEY}&center=${props.cityData.lat},${props.cityData.lon}&zoom=12`}/>
+            <img src={`https://maps.locationiq.com/v3/staticmap?key=${API_KEY}&center=${props.cityData.lat},${props.cityData.lon}&zoom=9`} style={{ width: '100%' }} alt="Map" />
         </>
     )
 }
