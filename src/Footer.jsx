f3e1eaa9783f429d4be8e463673d8e6d43914ccf
Justin Cheeseman <156013482+Cheeseman757@users.noreply.github.com>
2024-03-28 15:10:43 -0700

    Create Footer.jsx

---
+++ src/Footer.jsx
@@ -0,0 +1,12 @@
+
+function Footer() {
+
+    return (
+        <div style={{marginTop: '50px'}}>
+            <p> Made by Cheeseman </p>
+        </div>
+        
+    )
+}
+
+export default Footer;
